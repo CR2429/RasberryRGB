626b4196c2a01b5efad7507173bc573561f49979
CR2429 <guithopaoli@gmail.com>
2024-11-28 20:36:51 -0500

    test du joystick

--- Python/main.py
+++ Python/main.py
@@ -13,21 +13,24 @@ def loopX():
     thread qui gere l'axe X du joystick (couleur)
     """
     while True:
-        print("1")
+        print(f'valeur x : {adc.analogRead(0)}')
+        time.sleep(1)
 
 def loopY():
     """
     thread qui gere l'axe Y du joystick (mode)
     """
     while True:
-        print("2")
+        print(f'valeur y : {adc.analogRead(1)}')
+        time.sleep(1)
 
 def loopZ():
     """
     thread qui gere le bouton du joystick (on/off)
     """
     while True:
-        print("3")
+        print(f'valeur z : {not bouton.value}')
+        time.sleep(1)
 
 def destroy():
     """
