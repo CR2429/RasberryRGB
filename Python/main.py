23244d0560353a53b3c7fac11ce9886b893c1169
CR2429 <guithopaoli@gmail.com>
2024-11-28 22:20:16 -0500

    Calibrage

--- Python/main.py
+++ Python/main.py
@@ -8,6 +8,16 @@ bouton = Button(4) #le bouton du joystick qui es sur la pin 7 = GPIO4
 adc = ADS7830() #Ceci est la class qui controle ma puce ADC
 adc_lock = threading.Lock() #ceci est pour eviter les conflit entre les threads dans la lecture de la puce ADC
 
+#Valeur de calibrage
+VALEUR_CENTRAL_X = 0
+VALEUR_CENTRAL_Y = 0
+SEUIL_HAUT = 0
+SEUIL_BAS = 0
+SEUIL_GAUCHE = 0
+SEUIL_DROIT = 0
+
+
+
 def loopX():
     """
     thread qui gere l'axe X du joystick (couleur)
@@ -109,6 +119,17 @@ def destroy():
 ##demarrage des threads
 if __name__ == '__main__':
     print('Demarrage du serveur en cours')
+    print('Calibration en cours')
+    VALEUR_CENTRAL_X = adc.analogRead(0)
+    print(f'VALEUR_CENTRAL_X = {VALEUR_CENTRAL_X}')
+    VALEUR_CENTRAL_Y = adc.analogRead(1)
+    print(f'VALEUR_CENTRAL_Y = {VALEUR_CENTRAL_Y}')
+    SEUIL_HAUT = VALEUR_CENTRAL_X + 80
+    SEUIL_BAS = VALEUR_CENTRAL_X - 80
+    SEUIL_DROIT = VALEUR_CENTRAL_Y + 80
+    SEUIL_GAUCHE = VALEUR_CENTRAL_Y - 80
+    print('Calibrage terminer')
+    
     try:
         threadX = threading.Thread(target=loopX)
         threadY = threading.Thread(target=loopY)
@@ -117,6 +138,7 @@ if __name__ == '__main__':
         threadX.start()
         threadY.start()
         threadZ.start()
+        print('Serveur ouvert')
         
     except KeyboardInterrupt:
         destroy()
