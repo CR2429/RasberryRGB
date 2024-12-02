b8ae08e09789f1bd54a0244891d56e11fbd3c429
CR2429 <wdgaster.gtp@gmail.com>
2024-12-02 10:23:13 -0500

    voir dans la console

--- Python/main.py
+++ Python/main.py
@@ -42,11 +42,11 @@ def runTests():
     while True:
         #run les tests
         test = unittest.TestLoader().loadTestsFromTestCase(Test)
-        
+        unittest.TextTestRunner().run(test)
         #log
-        with open("test_output.log", "w", encoding="utf-8") as file:
-            runner = unittest.TextTestRunner(stream=file, verbosity=2)
-            runner.run(test)
+        #with open("test_output.log", "w", encoding="utf-8") as file:
+        #    runner = unittest.TextTestRunner(stream=file, verbosity=2)
+        #    runner.run(test)
  
         time.sleep(1)
 
