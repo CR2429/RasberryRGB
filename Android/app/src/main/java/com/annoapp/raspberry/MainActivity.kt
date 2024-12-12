39d596554c4a03ed114faf6e37ddba9bdcae78bd
CR2429 <wdgaster.gtp@gmail.com>
2024-12-12 12:37:37 -0500

    menu

--- Android/app/src/main/java/com/annoapp/raspberry/MainActivity.kt
+++ Android/app/src/main/java/com/annoapp/raspberry/MainActivity.kt
@@ -2,10 +2,30 @@ package com.annoapp.raspberry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    /** Surcharge pour la creation du menu */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    /** Surcharge des interactions des items du menu */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menuSettings -> {
+
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
