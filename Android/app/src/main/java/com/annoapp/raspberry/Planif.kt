8a13244686c19e1742b1544c36bdf906cda3db3d
CR2429 <wdgaster.gtp@gmail.com>
2024-12-13 16:12:54 -0500

    classe planif

---
+++ Android/app/src/main/java/com/annoapp/raspberry/Planif.kt
@@ -0,0 +1,32 @@
+package com.annoapp.raspberry;
+
+import androidx.annotation.RequiresApi;
+import android.os.Build;
+
+import java.io.Serializable;
+import java.sql.Time
+import java.util.UUID;
+
+@RequiresApi(Build.VERSION_CODES.O)
+class Planif(private var Titre: String, private var Heure: Time) :Serializable {
+    //Propriete
+    private val ID = UUID.randomUUID().toString();
+
+    // get-set
+    fun getID(): String {
+        return ID
+    }
+    fun getTitre() : String {
+        return Titre
+    }
+    fun setTitre(titre:String) {
+        this.Titre = titre
+    }
+    fun getHeure() : Time {
+        return Heure
+    }
+    fun setHeure(heure:Time) {
+        this.Heure = heure
+    }
+
+}
