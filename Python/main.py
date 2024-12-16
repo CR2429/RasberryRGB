8245da331a3b97426ab3b78077214d43e4fd86ea
CR2429 <wdgaster.gtp@gmail.com>
2024-12-16 10:04:57 -0500

    0.0.0.0

--- Python/main.py
+++ Python/main.py
@@ -86,7 +86,7 @@ if __name__ == '__main__':
         
         #serveur get/post
         handler = MyHttpRequestHandler
-        serveur = HTTPServer(('localhost', 4443), handler)
+        serveur = HTTPServer(('0.0.0.0', 4443), handler)
         serveur.socket = ssl.wrap_socket(serveur.socket, keyfile='cle.pem', certfile='certificat.pem', server_side=True)
         serveur.serve_forever()
         
